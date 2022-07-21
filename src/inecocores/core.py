88714ded73bb0fb063ab23cfbacb502042588082
tititab <thitithup@gmail.com>
2022-07-21 16:00:59 +0700

    Modify readme.md

--- src/inecocores/core.py
+++ src/inecocores/core.py
@@ -43,11 +43,11 @@ class Service:
 
 
 # if __name__ == "__main__":
-    s = Service()
-    s.check_report(customerId='1', reportId="123", parameters={"a": 1, "b": 2, "c": 3})
-    sample_parameter = {
-        'js_parameter_name': 'IDS',
-        'js_parameter_value': 'sale_order.id = 32069',
-    }
-    result = s.call_report(customerId='1', reportId="123", parameters=sample_parameter)
-    print(result['output'])
+#     s = Service()
+#     s.check_report(customerId='1', reportId="123", parameters={"a": 1, "b": 2, "c": 3})
+#     sample_parameter = {
+#         'js_parameter_name': 'IDS',
+#         'js_parameter_value': 'sale_order.id = 32069',
+#     }
+#     result = s.call_report(customerId='1', reportId="123", parameters=sample_parameter)
+#     print(result['output'])
